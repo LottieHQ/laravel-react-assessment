d45289c34cf52fffa7c83f348eb88d287f311f7a
Oscar Wales <oscarjwales@gmail.com>
2023-03-25 19:20:30 +0000

    Moved from TS to JS

---
+++ webapp/src/List/List.js
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+import { Container, Title } from "./listStyles";
+
+const List = () => {
+  return (
+    <Container>
+      <Title>Locations</Title>
+    </Container>
+  );
+};
+
+export default List;
---
+++ webapp/src/utils/styleVariables.js
@@ -0,0 +1,9 @@
+export const palette = {
+  orange: "#f77a26",
+  navy: "#253048",
+  grey: "#c2ccc3",
+  red: "#e73f1e",
+  black: "#1a1a1a",
+  yellow: "#eeff41",
+  cream: "#f8ede7",
+};
