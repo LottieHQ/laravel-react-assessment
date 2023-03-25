d6037f90569436affb38824fce0acfd60455dcda
Oscar Wales <oscarjwales@gmail.com>
2023-03-25 16:26:25 +0000

    Moved BE into dedicated folder

---
+++ api/app/Http/Resources/API/LocationResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources\API;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class LocationResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            "id" => $this->id,
+            "date_start" => $this->date_start,
+            "date_end" => $this->date_end,
+            "status" => $this->status,
+            "location_name" => $this->location_name,
+            "location_description" => $this->location_description,
+        ];
+    }
+}
