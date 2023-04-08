859d472950046250ec6dbb2bf810fbc56407fdcc
Niz97 <97niran@gmail.com>
2023-04-08 18:35:52 +0100

    Added casts for date datetime fields

--- server/app/Models/Location.php
+++ server/app/Models/Location.php
@@ -9,6 +9,13 @@ class Location extends Model
 {
   use HasFactory;
 
+  protected $casts = [
+    'created_at' => 'datetime:Y-m-d H:i:s',
+    'updated_at' => 'datetime:Y-m-d H:i:s',
+    'date_start' => 'date:Y-m-d',
+    'date_end' => 'date:Y-m-d',
+  ];
+
   const OPEN = 'open';
   const CLOSED = 'closed';
 
