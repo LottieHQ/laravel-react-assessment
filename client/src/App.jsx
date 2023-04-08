20638d86d88a17ab519136c56ce9b732f608cbc8
Niz97 <97niran@gmail.com>
2023-04-08 18:44:58 +0100

    Client maps over locations stored in state and renders to screen

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,35 @@
 import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [locations, setLocations] = useState([])
 
   useEffect(() => {
-    async function fetchMyAPI() {
+    async function fetchLocations() {
       let response = await fetch("http://127.0.0.1:8000/api/locations");
       let result = await response.json()
-      setCount(result)
+      setLocations(result)
     }
 
-    fetchMyAPI()
+    fetchLocations()
   }, [])
 
   return (
-    <div className="App">
-      Lottie assessment
+    <div className="screen">
+      {locations.map((location) => (
+        <div 
+          className="location"
+          key={`location-key-${location.id}`}
+        >
+          <h3>{location.name}</h3>
+          <h4>{location.description}</h4>
+          <h4>{location.status}</h4>
+          <h5>{location.date_start}</h5>
+          <h5>{location.date_end}</h5>
+
+        </div>
+      ))}
+
     </div>
   )
 }
