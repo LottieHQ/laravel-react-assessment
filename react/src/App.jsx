4b0b172791b9c9a7463308a0bca208f48203dc73
Ginnie Richichi <ginnie.richichi@datatank.co.uk>
2023-04-19 15:24:22 +0100

    updated button name

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -58,7 +58,7 @@ export default function App() {
                         type="button"
                         className="block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 >
-                    Add user
+                    Add Location
                 </button>
 
                 <Modal title="Create New Location" onClose={() => setShow(false)} show={show}>
